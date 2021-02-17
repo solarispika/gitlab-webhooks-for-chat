c75f4bedc3d42e38362d64993c18b0a86ed0f76a
Sung, Po Han <bernies@synology.com>
2021-02-17 14:49:38 +0800

    Refactor hook sending

--- index.js
+++ index.js
@@ -5,6 +5,34 @@ const webhooks = new WebhooksApi({
   secret: 'mysecret'
 })
 
+function send_hook(data)
+{
+  const options = {
+    hostname: 'chat.synology.com',
+    path: '/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22A33vUQlfN4ETsuJonT7pVsYxiDbhPWVnCG078pHQ1JGEdSo8zKqWuAkQ3JzTT2LQ%22',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(data)
+    }
+  };
+  const req = https.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+  });
+  req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  // Write data to request body
+  req.write(data);
+  req.end();
+}
+
 webhooks.on('Push Hook', ({id, name, payload}) => {
   const {before, after, user_name, project: { web_url, namespace, name: project_name }} = payload
   const branch_name = payload.ref.split('/').pop()
@@ -17,30 +45,7 @@ webhooks.on('Push Hook', ({id, name, payload}) => {
   const msg = `${user_name} pushed to branch ${branch_link} of ${project_link} (${compare_link})\n${commits}`
   const data = `payload={"text": "${msg}"}`
 
-  const options = {
-    hostname: 'chat.synology.com',
-    path: '/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22A33vUQlfN4ETsuJonT7pVsYxiDbhPWVnCG078pHQ1JGEdSo8zKqWuAkQ3JzTT2LQ%22',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(data)
-    }
-  };
-  const req = https.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
-    });
-  });
-  req.on('error', (e) => {
-    console.error(`problem with request: ${e.message}`);
-  });
-
-  // Write data to request body
-  req.write(data);
-  req.end();
+  send_hook(data)
 })
 
 webhooks.on('Tag Push Hook', ({id, name, payload}) => {
@@ -50,30 +55,7 @@ webhooks.on('Tag Push Hook', ({id, name, payload}) => {
   const msg = `${user_name} pushed new tag <${tag_url}|${tag_name}> to <${web_url}|${namespace} / ${project_name}>`
   const data = `payload={"text": "${msg}"}`
 
-  const options = {
-    hostname: 'chat.synology.com',
-    path: '/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22A33vUQlfN4ETsuJonT7pVsYxiDbhPWVnCG078pHQ1JGEdSo8zKqWuAkQ3JzTT2LQ%22',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(data)
-    }
-  };
-  const req = https.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
-    });
-  });
-  req.on('error', (e) => {
-    console.error(`problem with request: ${e.message}`);
-  });
-
-  // Write data to request body
-  req.write(data);
-  req.end();
+  send_hook(data)
 })
 
 /**
@@ -110,30 +92,7 @@ webhooks.on('Pipeline Hook', ({id, name, payload}) => {
   ].map(m => `> ${m}`).join('\n')
   const data = `payload={"text": "${msgs}"}`
 
-  const options = {
-    hostname: 'chat.synology.com',
-    path: '/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22A33vUQlfN4ETsuJonT7pVsYxiDbhPWVnCG078pHQ1JGEdSo8zKqWuAkQ3JzTT2LQ%22',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(data)
-    }
-  };
-  const req = https.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
-    });
-  });
-  req.on('error', (e) => {
-    console.error(`problem with request: ${e.message}`);
-  });
-
-  // Write data to request body
-  req.write(data);
-  req.end();
+  send_hook(data)
 });
 
 http.createServer(webhooks.middleware).listen(3000)
